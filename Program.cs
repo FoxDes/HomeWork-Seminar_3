bfadbff3bc7b54f4567b66db18873d6056dacef4
Denis Malyarenko <mdn-fox@yandex.ru>
2022-08-15 13:15:54 +0500

    выполнил третью задачу

--- Program.cs
+++ Program.cs
@@ -19,24 +19,40 @@
 // Напишите программу, которая принимает на вход координаты двух точек 
 // и находит расстояние между ними в 3D пространстве.
 
-Console.WriteLine();
-int xA = Coordinate("x", "A");
-int yA = Coordinate("y", "A");
-int zA = Coordinate("z", "A");
-int xB = Coordinate("x", "B");
-int yB = Coordinate("y", "B");
-int zB = Coordinate("z", "B");
-int Coordinate(string Axis, string Point)
+// Console.Clear();
+// Console.WriteLine();
+// int xA = Coordinate("x", "A");
+// int yA = Coordinate("y", "A");
+// int zA = Coordinate("z", "A");
+// int xB = Coordinate("x", "B");
+// int yB = Coordinate("y", "B");
+// int zB = Coordinate("z", "B");
+// int Coordinate(string Axis, string Point)
+// {
+//     Console.Write($"Введите координату {Axis} точки {Point}: ");
+//     return Convert.ToInt16(Console.ReadLine());
+// }
+// double Decision(double xA, double xB, 
+//                 double yA, double yB, 
+//                 double zA, double zB){
+//   return Math.Sqrt(Math.Pow((xB-xA), 2) + 
+//                    Math.Pow((yB-yA), 2) + 
+//                    Math.Pow((zB-zA), 2));
+// }
+// double Result =  Math.Round (Decision(xA, xB, yA, yB, zA, zB), 2 );
+// Console.WriteLine($"Расстояние между точками  {Result}");
+
+
+
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (i <= N)
 {
-    Console.Write($"Введите координату {Axis} точки {Point}: ");
-    return Convert.ToInt16(Console.ReadLine());
+    Console.WriteLine($"{Math.Pow(i, 3)}");
+    i++;
 }
-double Decision(double xA, double xB, 
-                double yA, double yB, 
-                double zA, double zB){
-  return Math.Sqrt(Math.Pow((xB-xA), 2) + 
-                   Math.Pow((yB-yA), 2) + 
-                   Math.Pow((zB-zA), 2));
-}
-double Result =  Math.Round (Decision(xA, xB, yA, yB, zA, zB), 2 );
-Console.WriteLine($"Расстояние между точками  {Result}");
